557c1239aff75ab27c23dd308400b4722b9131cf
monsosam <monsonsamuel5@gmail.com>
2023-09-18 18:28:40 -0600

    this project

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+let currQuestion = 0
+let score = 0
+ 
+
+ 
+function loadScore() {
+    const totalScore = document.getElementById("score")
+    totalScore.textContent = `You scored ${score} out of ${Questions.length}`
+}
+ 
+ 
+function nextQuestion() {
+    if (currQuestion < Questions.length - 1) {
+        currQuestion++;
+        loadQues();
+    } else {
+        document.getElementById("opt").remove()
+        document.getElementById("ques").remove()
+        document.getElementById("btn").remove()
+        loadScore();
+    }
+}
+ 
+function checkAns() {
+    const selectedAns = parseInt(document.querySelector('input[name="answer"]:checked').value);
+ 
+    if (Questions[currQuestion].a[selectedAns].isCorrect) {
+        score++;
+        console.log("Correct")
+        nextQuestion();
+    } else {
+        console.log("Wrong")
+        nextQuestion();
+    }
+}
